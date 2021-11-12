1c41ef0ab3d1a42d36e0716b1ac9a470a525d5b5
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-11-11 22:04:50 -0500

    Add two text fields in TextControl

--- src/components/TextControl.js
+++ src/components/TextControl.js
@@ -1,5 +1,38 @@
-import React from "react"
+import React, { useState, useContext, useEffect } from "react"
+import Box from "@mui/material/Box"
+import TextField from "@mui/material/TextField"
+import { AppContext } from "../context"
 
 export default function TextControl() {
-  return <div className="text-control-holder">Text control area</div>
+  const { randomQuote } = useContext(AppContext)
+  const [author, setAuthor] = useState(randomQuote.quoteAuthor)
+  const [text, setText] = useState(randomQuote.quoteText)
+
+  // useEffect (()=>{
+
+  // })
+
+  const handleAuthor = event => {
+    setAuthor(event.target.value)
+  }
+
+  const handleText = event => {
+    setText(event.target.value)
+  }
+
+  return (
+    <div className="text-control-holder">
+      Text control area
+      <TextField
+        id="outlined-multiline-flexible"
+        name="author"
+        label="Author"
+        // multiline
+        // maxRows={4}
+        value={author}
+        onChange={handleAuthor}
+      />
+      <TextField id="outlined-multiline-static" label="Text" multiline rows={4} value={text} onChange={handleText} />
+    </div>
+  )
 }
