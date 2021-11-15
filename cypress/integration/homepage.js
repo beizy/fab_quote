d001708a95c594ad4925896243592d0ca0cdfbb6
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-11-14 21:13:11 -0500

    Test new random quote and random image buttons

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -42,6 +42,24 @@ describe("homepage", () => {
     cy.get(".quote-text").contains("Be yourself; everyone else is already taken.")
   })
 
+  it("When User clicks 'New Random Image' button, User should see a different image", () => {
+    cy.intercept("GET", "https://api.unsplash.com/photos/random?query=quote+background&orientation=landscape", {
+      fixture: "randomBackground2.json",
+    })
+      .as("getNewRandomImage")
+      .get(".button-holder")
+      .first()
+      .next()
+      .click()
+
+    cy.get(".post")
+      .should("have.css", "background-image")
+      .and(
+        "eq",
+        'url("https://images.unsplash.com/photo-1469474968028-56623f02e42e?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MnwyNzQ2MTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzY1NTk4OTE\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=1080")'
+      )
+  })
+
   // it("As a user, I can click a movie, and see that movie’s details", () => {
   //   cy.get("article").contains("Mulan").click().url().should("include", "337401")
   // })
