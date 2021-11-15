f3522dc3a3e97ac14c99cccd13456041df3f0032
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-11-15 17:22:52 -0500

    Finish homepage style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,21 +4,28 @@ import "../styles/nav.css"
 import logoImg from "../images/cropped-resize.png"
 import FavoriteIcon from "@mui/icons-material/Favorite"
 import Tooltip from "@mui/material/Tooltip"
+import FormatPaintIcon from "@mui/icons-material/FormatPaint"
 
 export default function Nav() {
   return (
     <div className="nav">
-      <Link to="/" className="nav-links">
-        <img src={logoImg} />
-      </Link>
-      <Link to="/favorites" className="fav-link">
-        <Tooltip title="Favortite quote list" placement="bottom">
-          <FavoriteIcon style={{ color: "#880000" }} />
-        </Tooltip>
-      </Link>
-      <Link to="/diy" className="diy-link">
-        Make My Own
-      </Link>
+      <div className="logo">
+        <Link to="/" className="nav-links">
+          <img src={logoImg} />
+        </Link>
+      </div>
+      <div className="nav-icons">
+        <Link to="/favorites" className="fav-link">
+          <Tooltip title="Favortite quote list" placement="bottom">
+            <FavoriteIcon style={{ color: "#6f6b63" }} />
+          </Tooltip>
+        </Link>
+        <Link to="/diy" className="diy-link">
+          <Tooltip title="Tweak it further!" placement="bottom">
+            <FormatPaintIcon style={{ color: "#6f6b63" }} />
+          </Tooltip>
+        </Link>
+      </div>
     </div>
   )
 }
