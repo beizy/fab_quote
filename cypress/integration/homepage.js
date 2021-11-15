5f3ea4e54bd7d9f01269c714308cab9e8b1c9a26
Bei Zhang <73845209+lokiandfengshui@users.noreply.github.com>
2021-11-14 21:07:29 -0500

    Add fixtures to test. Test random quote and bg on page load

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -12,16 +12,35 @@ describe("homepage", () => {
   })
 
   it("Upon arrival, User should see a randomly generated picture", () => {
-    cy.get(".post").expect(".post").to.have.ownProperty("backgroundImange")
+    cy.get(".post")
+      .should("have.css", "background-image")
+      .and(
+        "eq",
+        'url("https://images.unsplash.com/photo-1581985430116-d8fba25256b0?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MnwyNzQ2MTB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzY5Mzk5MTg\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=1080")'
+      )
   })
 
-  // it("User should see a login link in the header section when the page loads", () => {
-  //   cy.get("header >a").contains("Login")
-  // })
+  it("Upon arrival, User should see a randomly generated quote", () => {
+    cy.get(".quote-text").contains("Either you run the day or the day runs you.")
+  })
 
-  // it("User should see all movies displayed on page when it is loaded", () => {
-  //   cy.get(".card-holder").children().should("have.length", 4)
-  // })
+  it("User should also see the author of the quote", () => {
+    cy.get(".quote-author").contains("Jim Rohn")
+  })
+
+  it("Quote should be in default color black", () => {
+    cy.get(".quote-author").should("have.css", "color").and("eq", "rgb(0, 0, 0)")
+  })
+
+  it("When User clicks 'New Random Quote' button, User should see a different quote", () => {
+    cy.intercept("GET", "http://api.quotable.io/random?maxLength=140", { fixture: "randomQuote2.json" })
+      .as("getNewRandomQuote")
+      .get(".button-holder")
+      .first()
+      .click()
+
+    cy.get(".quote-text").contains("Be yourself; everyone else is already taken.")
+  })
 
   // it("As a user, I can click a movie, and see that movie’s details", () => {
   //   cy.get("article").contains("Mulan").click().url().should("include", "337401")
